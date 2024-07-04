99a71cda866482d41f77133901c0c8fed3c8f272
Exxilom <exxilom@gmail.com>
2024-07-04 18:31:37 -0300

    reconfiguracion de db

--- config/db_config.php
+++ config/db_config.php
@@ -2,6 +2,6 @@
     define("DBHOST","localhost");
     define("DBNAME","jano");
     define("DBPORT",3306);
-    define("DBUSER","keysbank");
-    define("DBPASS","accountDB_keysbank_2021");
+    define("DBUSER","root");
+    define("DBPASS","");
 ?>
